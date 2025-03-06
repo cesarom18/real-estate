d0b6095cf454d446155dc96279186bd09b4c07f4
cesarom18 <cesarosses885@gmail.com>
2025-03-05 21:18:36 -0300

    Creacion modelo Property

---
+++ src/models/PropertyModel.js
@@ -0,0 +1,50 @@
+import { DataTypes } from "sequelize";
+
+import { sequelize } from "../config/db.js";
+
+export const Property = sequelize.define("Property",
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        numRooms: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1
+        },
+        numBaths: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        mt2: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        propertyStateId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        propertyTypeId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        tableName: "Properties",
+        timestamps: true
+    }
+);
--- src/models/associations.js
+++ src/models/associations.js
@@ -1,5 +1,6 @@
 import { PropertyState } from "./PropertyStateModel.js";
-import { PropertyType } from "./PropertyTypeModel.js"
+import { PropertyType } from "./PropertyTypeModel.js";
+import { Property } from "./PropertyModel.js";
 import { User } from "./UserModel.js";
 import { UserRol } from "./UserRolModel.js";
 
@@ -7,6 +8,17 @@ export const setupAssociations = () => {
     User.belongsTo(UserRol, {
         foreignKey: {
             name: "userRolId"
+        }
+    });
+
+    Property.belongsTo(PropertyState, {
+        foreignKey: {
+            name: "propertyStateId"
+        }
+    });
+    Property.belongsTo(PropertyType, {
+        foreignKey: {
+            name: "propertyTypeId"
         }
     });
 }
